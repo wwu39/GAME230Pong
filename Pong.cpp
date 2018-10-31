c91e7e741a014411937b51e2d84d34a17d8ae9d7
Weikai Wu <wwu39@ucsc.edu>
2018-10-30 18:57:42 -0700

    ball bounces

--- Pong.cpp
+++ Pong.cpp
@@ -1,12 +1,84 @@
 #include "Pong.h"
 
+#include <cmath>
 
+#define PI 3.14159265358979323846
+
+void Pong::update_state(float dt)
+{
+	ball.move(dt);
+}
+
+void Pong::render_frame()
+{
+	window.clear(Color(0, 0, 255));
+	window.draw(ball.shape);
+
+	Font font;
+	font.loadFromFile("font.ttf");
+	Text text;
+	text.setFont(font);
+	text.setCharacterSize(50);
+	text.setString("Hello, cowboy!");
+	text.setFillColor(Color::Green);
+	text.setPosition(50, 300);
+	window.draw(text);
+}
 
 Pong::Pong()
 {
+	window.create(VideoMode(RES_WIDTH, RES_HEIGHT), GAMETILE);
+	ball.shape.setPosition({ RES_WIDTH / 2.0f, RES_HEIGHT / 2.0f });
 }
 
 
 Pong::~Pong()
 {
+}
+
+int Pong::run()
+{
+	Clock clock;
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		float dt = clock.restart().asSeconds();
+
+		update_state(dt);
+		render_frame();
+		window.display();
+	}
+	return 0;
+}
+
+Ball::Ball()
+{
+	shape.setRadius(BALL_R);
+	shape.setOrigin({ BALL_R, BALL_R });
+
+	// random initial angle between 0-180
+	srand((unsigned)time(nullptr));
+	float angle = (float)(rand() % 180) / 180.0f * PI;
+	v = { cosf(angle), sinf(angle) };
+}
+
+void Ball::move(float dt)
+{
+	Vector2f pos = shape.getPosition();
+	pos += v;
+	if ((pos.x <= BALL_R && v.x < 0) || (pos.x >= RES_WIDTH - BALL_R && v.x > 0)) v.x = -v.x;
+	if ((pos.y <= BALL_R && v.y < 0) || (pos.y >= RES_HEIGHT - BALL_R && v.y > 0))v.y = -v.y;
+	shape.setPosition(pos);
+}
+
+Paddle::Paddle()
+{
+	shape.setSize({ PADDLE_W, PADDLE_H });
+	shape.setOrigin({ PADDLE_W / 2.0f, PADDLE_H / 2.0f });
 }
--- Pong.h
+++ Pong.h
@@ -1,20 +1,59 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/System.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML/Main.hpp>
+
+#define GAMETILE "Pong!"
+
+#define RES_WIDTH 800
+#define RES_HEIGHT 600
+
+#define BALL_R 10.0f
+#define PADDLE_W 25.0f
+#define PADDLE_H 100.0f
+
+using namespace sf;
 
 class Ball
 {
+	CircleShape shape;
+	Texture tex;
+	Sound hitsound;
+	Vector2f v;
+
 	friend class Pong;
-	friend class paddle;
+	friend class Paddle;
+
+public:
+	Ball();
+	void move(float dt);
 };
 
-class paddle
+class Paddle
 {
+	RectangleShape shape;
+	Texture tex;
 	friend class Pong;
+
+	Paddle();
 };
 
 class Pong
 {
+	Ball ball;
+	Paddle player1;
+	Paddle player2; // AI
+	RenderWindow window;
+
+	void update_state(float dt);
+	void render_frame();
 public:
 	Pong();
 	~Pong();
+	int run();
 };
 
