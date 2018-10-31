e4c8ef99df3fb14e2120736c272750864ddbe4eb
Weikai Wu <wwu39@ucsc.edu>
2018-10-30 23:42:54 -0700

    animated bomb

--- Pong.cpp
+++ Pong.cpp
@@ -1,8 +1,9 @@
 #include "Pong.h"
 
 #include <cmath>
+#include <sstream>
 
-#define PI 3.14159265358979323846
+#define PI 3.14159265358979323846f
 
 void Pong::update_state(float dt)
 {
@@ -12,14 +13,14 @@ void Pong::update_state(float dt)
 void Pong::render_frame()
 {
 	window.clear(Color(0, 0, 255));
-	window.draw(ball.shape);
+	ball.draw(window);
 
 	Font font;
 	font.loadFromFile("font.ttf");
 	Text text;
 	text.setFont(font);
 	text.setCharacterSize(50);
-	text.setString("Hello, cowboy!");
+	text.setString("Hello, Pong!");
 	text.setFillColor(Color::Green);
 	text.setPosition(50, 300);
 	window.draw(text);
@@ -28,7 +29,7 @@ void Pong::render_frame()
 Pong::Pong()
 {
 	window.create(VideoMode(RES_WIDTH, RES_HEIGHT), GAMETILE);
-	ball.shape.setPosition({ RES_WIDTH / 2.0f, RES_HEIGHT / 2.0f });
+	ball.setPosition(RES_WIDTH / 2.0f, RES_HEIGHT / 2.0f);
 }
 
 
@@ -57,24 +58,55 @@ int Pong::run()
 	return 0;
 }
 
+void Ball::animate()
+{
+	std::ostringstream filename;
+	filename << "sprites/bomb 00" << (curframe >= 10 ? "" : "0") << curframe << ".png";
+	tex.loadFromFile(filename.str());
+	shape.setTexture(&tex);
+	curframe = (curframe + 1) % 16;
+}
+
 Ball::Ball()
 {
 	shape.setRadius(BALL_R);
 	shape.setOrigin({ BALL_R, BALL_R });
+	curframe = 0;
+	rate = RATE;
 
 	// random initial angle between 0-180
 	srand((unsigned)time(nullptr));
 	float angle = (float)(rand() % 180) / 180.0f * PI;
-	v = { cosf(angle), sinf(angle) };
+	v = { BALL_SPEED * cosf(angle), BALL_SPEED * sinf(angle) };
 }
 
 void Ball::move(float dt)
 {
 	Vector2f pos = shape.getPosition();
+	if ((pos.x <= BALL_CR && v.x < 0) || (pos.x >= RES_WIDTH - BALL_CR && v.x > 0)) v.x = -v.x;
+	if ((pos.y <= BALL_CR && v.y < 0) || (pos.y >= RES_HEIGHT - BALL_CR && v.y > 0))v.y = -v.y;
 	pos += v;
-	if ((pos.x <= BALL_R && v.x < 0) || (pos.x >= RES_WIDTH - BALL_R && v.x > 0)) v.x = -v.x;
-	if ((pos.y <= BALL_R && v.y < 0) || (pos.y >= RES_HEIGHT - BALL_R && v.y > 0))v.y = -v.y;
 	shape.setPosition(pos);
+}
+
+void Ball::draw(RenderWindow &w)
+{
+	++rate;
+	if (rate / RATE) { 
+		animate();
+		rate = 0;
+	}
+	w.draw(shape);
+}
+
+void Ball::setPosition(float x, float y)
+{
+	shape.setPosition({ x, y });
+}
+
+Vector2f Ball::getPosition()
+{
+	return shape.getPosition();
 }
 
 Paddle::Paddle()
--- Pong.h
+++ Pong.h
@@ -11,10 +11,12 @@
 
 #define RES_WIDTH 800
 #define RES_HEIGHT 600
-
-#define BALL_R 10.0f
+#define BALL_R 30.0f // radius of the ball
+#define BALL_CR 12.0f // radius of the ball used in collision
 #define PADDLE_W 25.0f
 #define PADDLE_H 100.0f
+#define BALL_SPEED 2.0f
+#define RATE 10
 
 using namespace sf;
 
@@ -24,21 +26,24 @@ class Ball
 	Texture tex;
 	Sound hitsound;
 	Vector2f v;
-
-	friend class Pong;
-	friend class Paddle;
+	int curframe;
+	int rate;
+	void animate();
 
 public:
 	Ball();
 	void move(float dt);
+	void draw(RenderWindow&);
+	void setPosition(float x, float y);
+	Vector2f getPosition();
 };
 
 class Paddle
 {
 	RectangleShape shape;
 	Texture tex;
-	friend class Pong;
 
+public:
 	Paddle();
 };
 
@@ -51,6 +56,7 @@ class Pong
 
 	void update_state(float dt);
 	void render_frame();
+
 public:
 	Pong();
 	~Pong();
